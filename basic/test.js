e15d4d42babc914903d57807c9013f526b542855
saju-negi7 <sajunegi111@gmail.com>
2025-01-12 18:42:42 +0000

    done

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,20 @@
 console.log("saju")
+
+const accid = 123
+let accname = "shivansh"
+var accpass = "shiv" // we do not use var type variable now a days rather we use let variable
+city = "dehradun"
+
+/*
+we do not use var because of issue in block scope or functional scope which is these bracket => {}
+    used in for loop and etc
+*/
+// use this 
+console.log(accid)
+console.log(accname)
+console.log(accpass)
+console.log(city)
+
+// or use this (efficient)
+
+console.table([accid , accname , accpass , city])
